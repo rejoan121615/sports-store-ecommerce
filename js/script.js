ca0b8ec84a617ce1b128d3718221d13ebacc8730
Mohd Rejoan <wearecrx@gmail.com>
2021-04-04 10:13:56 +0600

    add offer clock ticker using js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
 'use strict'
+// ---------------------------------
+// offer clock 
+// ----------------------------------
+const commonAddress = ".container .promotion_wrapper .promotion_text .times"; 
+const HourTag = document.querySelector(
+    `${commonAddress} .hour`
+);
 
-console.log('this is a simple text');
+const MunitesTag = document.querySelector(`${commonAddress} .munite`);
+const SecoundTag = document.querySelector(`${commonAddress} .secound`);
+
+// set timer 
+
+const ClockTicker = () => {
+    let Clock = new Date();
+    HourTag.innerText = Clock.getHours();
+    MunitesTag.innerText = Clock.getMinutes();
+    SecoundTag.innerText = Clock.getSeconds();
+}
+
+// ClockTicker()
+setInterval(() => {
+    ClockTicker()
+}, 1000);
